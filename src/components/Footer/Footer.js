1cbe01177462991727648679e65e16292f793777
DilanjanaVm <dilanjanavimukthi48@gmail.com>
2023-02-15 16:13:21 +0530

    add new sections into landing page and responsive the page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,7 @@ import './footer.scss'
 import logo from '../../assets/img/logos/whitelogo.png';
 import {Link} from "react-router-dom";
 import {Button, Col, Input, Row} from "reactstrap";
+import * as Icon from 'react-feather'
 
 class Footer extends React.Component {
 
@@ -11,26 +12,59 @@ class Footer extends React.Component {
             <div className='footer'>
                 <div className='container'>
                     <Row className='py-5'>
-                        <Col md={3}>
-                            <img src={logo} alt=""/>
-                        </Col>
-                        <Col md={3}>
-                            <span>Melbourne</span>
-                            <p> Level 1, Unit 9 / 677 <br/> Springvale Road,<br/> Mulgrave VIC 3170</p>
 
-                            <span className='mt-5'>Colombo</span>
-                            <p> Level 4, 46/38, <br/> Navam Mawatha, <br/> Colombo 02, 00200</p>
+                        <Col md={3}>
+                            <h3>Explore</h3>
+                            <p>About</p>
+                            <p>How it work</p>
+                            <p>FAQs</p>
+                            <p>News</p>
+                            <p>Careers</p>
+                        </Col>
 
-                        </Col>
                         <Col md={3}>
-                            <span>Work inquiries</span>
-                            <p> Interested in working with us? <br/> hello@empite.com</p>
+                            <h3>Services</h3>
+                            <p>Regular cleaning</p>
+                            <p>One - off cleaning</p>
+                            <p>End of tenancy cleaning </p>
+                            <p>Deep cleaning</p>
+                            <p>After builders </p>
+                            <p>Commercial cleaning</p>
+                            <p>Plumber </p>
+                            <p>More services ...</p>
                         </Col>
+
                         <Col md={3}>
-                            <span>Stay in touch</span>
-                            <div className='sub-input'><Input/> <Button>Subscribe</Button></div>
+                            <h3>Available in</h3>
+
+                            <p>East London </p>
+                            <p> North London </p>
+                            <p>South East London </p>
+                            <p> South London</p>
+                            <p>More locations ...</p>
+                        </Col>
+
+                        <Col md={3}>
+                            <h3>Contact Us</h3>
+
+                            <p>+44 079 7628 228</p>
+                            <br/>
+                            <p>92 Laring Road, <br/>New York</p>
+                            <br/>
+                            <p>abc@gmail.com</p>
+
                         </Col>
                     </Row>
+                    <Row>
+                        <Col sm={12} lg={12} className="d-flex justify-content-end social-media-link">
+                            <Icon.Facebook size={26} fill='#414141' color='414141'/>
+                            <Icon.Instagram size={26}  color='#414141'/>
+                            <Icon.Youtube size={26}  color='#414141'/>
+                            <Icon.Twitter size={26}  fill='#414141' color='#414141'/>
+                            <Icon.Linkedin size={26}  fill='#414141' color='#414141'/>
+                        </Col>
+                    </Row>
+
                 </div>
             </div>
 
--- src/view/landing/Landing.js
+++ src/view/landing/Landing.js
@@ -5,6 +5,7 @@ import clean from '../../assets/img/cleaning Img.jpg'
 import * as Icon from 'react-feather'
 import {Button, Card, CardBody, Col, Row} from "reactstrap";
 import {Input} from 'semantic-ui-react'
+import Footer from "../../components/Footer/Footer";
 
 class Landing extends React.Component {
     state = {
@@ -41,14 +42,16 @@ class Landing extends React.Component {
                                     <CardBody>
                                         <Row className='filter-wrapper'>
                                             <Col md={4}>
-                                                <Input className='w-100' iconPosition='left' icon='search' placeholder='Search...'/>
+                                                <Input className='w-100' iconPosition='left' icon='search'
+                                                       placeholder='Search...'/>
                                             </Col>
                                             <Col md={4}>
-                                                <Input className='w-100' iconPosition='left' icon='calendar' placeholder='Search...'/>
+                                                <Input className='w-100' iconPosition='left' icon='calendar'
+                                                       placeholder='Search...'/>
                                             </Col>
                                             <Col md={3}>
-                                            <Button className='btn-common btn-calender'> Find your calender</Button>
-                                        </Col>
+                                                <Button className='btn-common btn-calender'> Find your calender</Button>
+                                            </Col>
                                         </Row>
 
 
@@ -58,6 +61,96 @@ class Landing extends React.Component {
                         </Row>
                     </div>
                 </div>
+
+
+                <div className="container service-list">
+                    <Row>
+                        <Col sm={12} md={12} lg={12} className='text-center'>
+                            <h1 className='service-topic'>Our Local house cleaning services</h1>
+                            <h4 className='service-sub-topic
+                            '>Lorem ipsum dolor sit amet consectetur</h4>
+                        </Col>
+                    </Row>
+
+                    <Row className="service-card-wrapper">
+                        <Col className="service-card" sm={12} md={12} lg={4}>
+                            <Card>
+                                <CardBody className='service-card-body'>
+                                 <span className="view-icon">
+                                       <span> <Icon.Instagram color='white' size={32}/></span>
+                                 </span>
+                                    <h1 className="service-name">
+                                        Lorem ipsum dolo
+                                    </h1>
+                                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                        elit. Ad, adipisci consequatur, dicta dolore doloremque dolorum ea eum ex
+                                        exercitationem.</p>
+                                </CardBody>
+                            </Card>
+                        </Col>
+
+                        <Col className="service-card" sm={12} md={12} lg={4}>
+                            <Card>
+                                <CardBody className='service-card-body'>
+                                 <span className="view-icon">
+                                       <span> <Icon.Instagram color='white' size={32}/></span>
+                                 </span>
+                                    <h1 className="service-name">
+                                        Lorem ipsum dolo
+                                    </h1>
+                                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                        elit. Ad, adipisci consequatur, dicta dolore doloremque dolorum ea eum ex
+                                        exercitationem.</p>
+                                </CardBody>
+                            </Card>
+                        </Col>
+
+                        <Col className="service-card" sm={12} md={12} lg={4}>
+                            <Card>
+                                <CardBody className='service-card-body'>
+                                 <span className="view-icon">
+                                       <span> <Icon.Instagram color='white' size={32}/></span>
+                                 </span>
+                                    <h1 className="service-name">
+                                        Lorem ipsum dolo
+                                    </h1>
+                                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                        elit. Ad, adipisci consequatur, dicta dolore doloremque dolorum ea eum ex
+                                        exercitationem.</p>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+
+
+                <div className="apply-online-section">
+                    <div className="container">
+                        <Row>
+                            <Col sm={12} md={12} lg={8}>
+                                <h1 className='apply-topic'>Join with Us, as a company or cleaner</h1>
+                                <p className='apply-subText'>Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                                    consectetur adipisicingur adipisicing
+                                    elit</p>
+
+                                <p className="apply-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Aspernatur autem cupiditate, dolores itaque labore libero officia quaerat ut. Ab
+                                    aperiam architecto, consectetur deleniti deserunt distinctio facilis fugit quibusdam
+                                    sed voluptatibus cupiditate, dolores itaque labore libero officia quaerat ut. Ab
+                                    aperiam architecto, consectetur deleniti deserunt distinctio facilis fugit quibusdam
+                                    sed voluptatibus!</p>
+
+                                <Button className='btn-common btn-apply-online'> Apply Online </Button>
+                            </Col>
+                            <Col sm={12} md={12} lg={4}>
+                                <img src={clean} alt="apply-online-img" className="img-apply-online"/>
+                            </Col>
+                        </Row>
+                    </div>
+
+                </div>
+
+                <Footer/>
 
             </div>
         );
