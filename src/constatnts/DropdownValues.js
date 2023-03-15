c790a76bb434c49a1693d04278cc94771a52ff58
Dilanjana <dilanjana@webmotech.com>
2023-03-15 01:40:16 -0700

    add basic files

---
+++ src/constatnts/DropdownValues.js
@@ -0,0 +1,5 @@
+export const userRoles = [
+    {value: 'ADMIN', label: 'Admin'},
+    {value: 'ADMIN_STAFF', label: 'Admin Staff'},
+    {value: 'CUSTOMER', label: 'customer'}
+]
---
+++ src/constatnts/TabeleColumDetails.js
@@ -0,0 +1,22 @@
+export const userDetailsColumns = [
+    {
+        name: 'First Name',
+        selector: 'firstName',
+        key: 'firstName'
+    },
+    {
+        name: 'Last Name',
+        selector: 'lastName',
+        key: 'lastName'
+    },
+    {
+        name: 'User Role',
+        selector: 'userRole',
+        key: 'userRole'
+    },
+    {
+        name: 'Email',
+        selector: 'email',
+        key: 'userRole'
+    }
+]
